d8e72749531f18533ae951c7456c9b225b83d21d
Mahrane <mahranezayati46@gmail.com>
2026-05-09 21:33:19 +0100

    fix gemini model

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const genAI = new GoogleGenerativeAI(
 );
 
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash"
+   model: "gemini-2.0-flash"
 });
 const client = new Client({
     intents: [
